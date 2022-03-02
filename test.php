9813e1a3ed38b33d7ba56d3eac9651ce83e6fe75
loc <bubblesfight99@gmail.com>
2022-03-02 20:57:00 +0700

    add Jquery

---
+++ test.php
@@ -0,0 +1,136 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./main.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <title> Bài Tập Jquery </title>
+</head>
+
+<body>
+<center>
+     <br>
+     <br>
+               <h1> các bài tập Jquery cơ bản  </h1>
+    <br>
+     <div>
+    <H2> BÀI 1 : Cách ẩn hiện nội dung khi click vào button trong jQuery </H2>
+
+        <div>
+            <lable><span>màu xanh lá</span><input class="selector__radio" type="radio" name="colorRadio" value="green"></lable>
+            <lable><span>màu vàng</span><input class="selector__radio" type="radio" name="colorRadio" value="yellow"></lable>
+            <lable><span>màu đỏ</span><input class="selector__radio" type="radio" name="colorRadio" value="red"></lable>
+        </div>
+
+        <div class="block__color">
+            <div class="green box"> Bạn đã chọn màu xanh lá  </div>
+            <div class="yellow box"> Bạn đã chọn màu vàng    </div>
+            <div class="red box"> Bạn đã chọn màu đỏ         </div>
+        </div>
+
+    <script>
+        $(document).ready(function (){
+            $('.box').hide();
+            $('input[type="radio"]').click(function (){
+                var inputValue = $(this).attr("value");
+                var targetBox = $("." + inputValue);
+                $('.box').hide();
+                $(targetBox).show();
+            })
+        })
+    </script>
+      </div>
+    <hr>
+     <div>
+         <br>
+         <H2> bài 2 : Tạo máy tính bằng Jquery  </H2>
+
+           <div class="calculator">
+               <input type="text" class="hover__item box__calculation" value="">
+               <input type="text" class=" hover__item box__result" value="" >
+
+               <div class="box-button">
+                   <button class=" hover__item refresh">CE</button>
+                   <button class=" hover__item del">Del</button>
+                   <button class=" hover__item cong operation">+</button>
+                   <button class=" hover__item tru operation">-</button>
+                   <button class=" hover__item nhan operation">*</button>
+                   <button class=" hover__item chia operation">/</button>
+                   <button class=" hover__item number">9</button>
+                   <button class=" hover__item number">8</button>
+                   <button class=" hover__item number">7</button>
+                   <button class=" hover__item number">6</button>
+                   <button class=" hover__item number">5</button>
+                   <button class=" hover__item number">4</button>
+                   <button class=" hover__item number">3</button>
+                   <button class=" hover__item number">2</button>
+                   <button class=" hover__item number">1</button>
+                   <button class=" hover__item number">0</button>
+                   <button class=" hover__item cham">.</button>
+                   <button class=" hover__item equal">=</button>
+
+               </div>
+           </div>
+
+         <script>
+             $(document).ready(function (){
+                 // vô hiệu hóa các nút
+                 $('.box__calculation').attr('disabled','true'); // Vô hiệu hoá ô phép tính
+                 $('.box__result').attr('disabled','true'); // Vô hiệu hoá ô kết quả
+                 $('.box-button .nhan').attr('disabled','true'); // vô hiệu hóa nút X
+                 $('.box-button .chia').attr('disabled','true'); // vô hiệu hóa nút /
+                 $('.box-button .equal').attr('disabled','true'); // vô hiệu hóa nút =
+
+                 //click vào các nút số
+                 $('.number').on('click', function (){
+                     var char = $(this).html(); // lấy số vừa click
+                     var value_calculation = $('.box__calculation').val();// Lấy chuỗi giá trị trong ô phép tính
+                     $('.box__calculation').val(value_calculation + char); // Thêm số vừa click vào sau chuỗi giá trị trong ô phép tính
+                     $('.operation').removeAttr('disabled'); //gỡ vô hiệu hóa các nút X /
+                     $('.equal').removeAttr('disabled'); //gỡ vô hiệu hóa các nút =
+                     $('.cham').removeAttr('disabled');// gỡ vô hiệu hóa nút .
+                 })
+
+                 // click nút " . "
+                 $('.cham').on('click',function (){
+                     var char = $(this).html();// lấy dấu chấm
+                     var value_calculation = $('.box__calculation').val();// lấy value ô tính
+                     $('.box__calculation').val(value_calculation + char);// thêm dấu . vào ô tính
+                     $('.operation').attr('disabled','true');// vô hiệu hóa các nút tính toán
+                     $(this).attr('disabled','true');//vô hiệu hóa nút chấm
+                     console.log(char);
+                 })
+
+                 // click nút CE
+                 $('.refresh').on('click', function (){
+                     $('.box__calculation').val('');// làm trống ô phép tính
+                     $('.box__result').val('');//là trống ô kết quả
+                 })
+                 // click nút DEL
+                 $('.del').on('click', function (){
+                     var string = $('.box__calculation').val();
+                     $('.box__calculation').val(string.slice(0, -1)); //xóa ký tự cuối trong ô tính
+                     $
+                 })
+                 // click nút phép toán
+                 $('.operation').on('click', function (){
+                     var char = $(this).html();
+                     var value_calculation = $('.box__calculation').val();
+                     $('.box__calculation').val(value_calculation + char);
+                     $('.nhan').attr('disabled', 'true'); // vô hiệu hóa nút X
+                     $('.chia').attr('disabled', 'true'); // vô hiệu hóa nút /$('.dot').removeAttr('disabled');
+                     $('.equal').attr('disabled','true');//vô hiệu hóa các nút =
+                 })
+                 //click NÚT =
+                 $(".equal").on('click', function (){
+                     var result = $('.box__calculation').val();
+                     $('.box__result').val(eval(result));
+                 })
+             })
+         </script>
+     </div>
+<hr>
+
+</center>
+</body>
+</html>
